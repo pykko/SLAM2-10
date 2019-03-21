5e4a9480f5efd9876bbf2a394215dd50640b8de1
pykko <pykko@xilim.fr>
2019-03-21 11:02:58 +0100

    Classe AppGGNS

---
+++ AppGGNS.java
@@ -0,0 +1,70 @@
+package fr.gns.gestion ;
+
+public class AppGGNS {
+
+	
+	public static void main(String[] args) {
+		
+		// Question 6 - À décommenter
+		
+//		Joueur joueur1201 = new Joueur( 1201 , "TEFFON" , "Yvan" ) ;
+//		System.out.println( joueur1201 ) ;
+//		Joueur joueur502 = new Joueur( 502 , "KERREH" , "Armelle" ) ;
+//		System.out.println( joueur502 ) ;
+//		Joueur joueur1823 = new Joueur( 1823 , "MATOS" , "Louis" ) ;
+//		System.out.println( joueur1823 ) ;
+//		Joueur joueur835 = new Joueur( 835 , "AIT EL KABOUS" , "Fatima" ) ;
+//		System.out.println( joueur835 ) ;
+//		Joueur joueur2920 = new Joueur( 2920 , "BOUVIN" , "Karine" ) ;
+//		System.out.println( joueur2920 ) ;
+//		Joueur joueur1010 = new Joueur( 1010 , "GENPRI" , "Erwan" ) ;
+//		System.out.println( joueur1010 ) ;
+//		Joueur joueur1011 = new Joueur( 1011 , "GUETTA" , "David" ) ;
+//		System.out.println( joueur1011 ) ;
+		
+		// Question 7 - À décommenter
+		
+//		Jeu jeu23 = new Jeu( 23 , "Waterloo" , true ) ;
+//		System.out.println( jeu23 ) ;
+		
+		// Exercice 1 - À décommenter
+		
+//		jeu23.evaluer( joueur1201, new Evaluation( 5 , "Excellent !!!" ) ) ;
+//		jeu23.evaluer( joueur502, new Evaluation( 1 , "Vraiment pas bon" ) ;
+//		jeu23.evaluer( joueur1823, new Evaluation( 2 , "Bof" ) ) ;
+//		jeu23.evaluer( joueur835, new Evaluation( 2 , "La déception :(" ) ) ;
+//		jeu23.evaluer( joueur2920, new Evaluation( 5 , "Bravo") ) ;
+		
+		
+		System.out.println( "\n\n--[Exercice 2]--\n" ) ;
+		// Exercice 2 - Votre code ici
+		
+		System.out.println( "\n\n--[Exercice 3]--\n" ) ;
+		// Exercice 3 - Votre code ici
+		
+		System.out.println( "\n\n--[Exercice 4]--\n" ) ;
+		// Exercice 4 - Votre code ici
+		
+		System.out.println( "\n\n--[Exercice 5]--\n" ) ;
+		// Exercice 5 - Votre code ici
+		
+		System.out.println( "\n\n--[Exercice 6]--\n" ) ;
+		// Exercice 6 - Votre code ici
+		
+		System.out.println( "\n\n--[Exercice 7]--\n" ) ;
+		// Exercice 7 - Votre code ici
+		
+		System.out.println( "\n\n--[Exercice 8]--\n" ) ;
+		// Exercice 8 - Votre code ici
+		
+		System.out.println( "\n\n--[Exercice 9]--\n" ) ;
+		// Exercice 9 - Votre code ici
+		
+		System.out.println( "\n\n--[Exercice 10]--\n" ) ;
+		// Exercice 10 - Votre code ici
+		
+		System.out.println( "\n\n--[Exercice 11]--\n" ) ;
+		// Exercice 11 - Votre code ici
+	}
+
+}
